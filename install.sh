8b03d4d992fb6b4a6e3389d114dbb117d968bfa9
macrodrigues <marcoacavaco.rodrigues@hotmail.com>
2023-08-14 20:04:31 -0600

    initial commit

---
+++ install.sh
@@ -0,0 +1,11 @@
+pip install requirements.txt
+
+sudo apt-get install libasound2
+sudo apt-get install libgbm1
+sudo apt-get install libxdamage1
+sudo apt-get install libatspi2.0-0
+sudo apt-get install libxkbcommon0
+sudo apt-get install libatk-bridge2.0-0
+sudo apt-get install libatk1.0-0
+
+playwright install
