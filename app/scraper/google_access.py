7097bab05e69fa9897c0f33978ccc24eb5e86317
macrodrigues <marcoacavaco.rodrigues@hotmail.com>
2023-09-11 23:47:25 +0200

    Several changes and code optimization

---
+++ app/scraper/google_access.py
@@ -0,0 +1,56 @@
+""" Script containing the Google interaction functions.
+
+One for authentication, one for reading and one for uploading.
+
+"""
+import pandas as pd
+import gspread
+from google.oauth2.service_account import Credentials
+
+
+def google_authentication(credentials, sheet_id) -> object:
+    """ This function gives authentication to the Google Account.
+
+    Scopes defines the permissions to Googe Sheets and Google Drive.
+    Than, using gspread, it authorizes the authentication and
+    opens the the worksheet to work with.
+
+    """
+    # Authenticate with Google Sheets using the JSON key file
+    scope = ['https://www.googleapis.com/auth/spreadsheets',
+             'https://www.googleapis.com/auth/drive']
+
+    creds = Credentials.from_service_account_file(
+        credentials, scopes=scope)
+
+    client = gspread.authorize(creds)
+
+    # Open the Google Sheet by id
+    sheet = client.open_by_key(sheet_id)
+    work_sheet = sheet.get_worksheet(0)
+    return work_sheet
+
+
+def read_from_google(sheet) -> pd.DataFrame:
+    """ This function reads the data from the Google Sheet. """
+    df_previous = pd.DataFrame(data=sheet.get_all_records())
+    return df_previous
+
+
+def upload_to_google(df, sheet):
+    """ This function uploads the data to a google sheet.
+
+    It takes the worksheet object created in google_authentication(), and
+    writes data from a dataframe to it.
+
+    """
+    # Convert the DataFrame to a list of lists
+    data = df.values.tolist()
+
+    # Clear existing data and update the Google Sheet with new data
+    sheet.clear()
+    # Convert the DataFrame headers to a list and insert as the first row
+    header_row = df.columns.tolist()
+    sheet.insert_rows([header_row], row=1)
+    # insert the full data
+    sheet.insert_rows(data, row=2)
--- app/scraper/scraper.py.py
+++ app/scraper/scraper.py.py
@@ -1,83 +1,71 @@
 """ this script scrapes villas and lands from a property website."""
 
+# pylint: disable=broad-except
 import os
 import time
 import re
 import datetime as dt
 from dotenv import load_dotenv
-import pandas as pd
-import gspread
-from google.oauth2.service_account import Credentials
 from playwright.sync_api import sync_playwright
+from google_access import pd, google_authentication
+from google_access import upload_to_google, read_from_google
 from bs4 import BeautifulSoup
 
 
-def upload_to_google(json_path, sheet_id, df):
-    """ this function uploads data to a google sheet.
+def get_last_page_number(page, url) -> int:
+    """This function looks for the last page to scrape.
 
-    It takes the service account credentials.json to make an object that
-    connects with gspread to make a client.
-
-    Then it saves the dataframe into the google sheet.
+    By looking for the pagination id, it finds the page-item elements.
+    These are converted to a list, then the function takes the
+    last number shown on the pagination.
 
     """
-    # Authenticate with Google Sheets using the JSON key file
-    scope = ['https://www.googleapis.com/auth/spreadsheets',
-             'https://www.googleapis.com/auth/drive']
+    # go to url with Playwright page element
+    page.goto(url)
 
-    creds = Credentials.from_service_account_file(
-        json_path, scopes=scope)
+    # tackle only the #box id
+    html = page.inner_html('#pagination')
 
-    client = gspread.authorize(creds)
+    # create beautiful soup element
+    soup = BeautifulSoup(html, 'html.parser')
 
-    # Open the Google Sheet by id
-    sheet = client.open_by_key(sheet_id)
-    sheet1 = sheet.get_worksheet(0)
+    # Find all elements with the class "box property-item"
+    page_items = soup.find_all('li', {'class': 'page-item'})
 
-    # Convert the DataFrame to a list of lists
-    data = df.values.tolist()
-
-    # Clear existing data and update the Google Sheet with new data
-    sheet1.clear()
-    # Convert the DataFrame headers to a list and insert as the first row
-    header_row = df.columns.tolist()
-    sheet1.insert_rows([header_row], row=1)
-    # insert the full data
-    sheet1.insert_rows(data, row=2)
+    return int(page_items[-2].text)
 
 
-def update_dataframe(df_new, df_old=None):
+def update_dataframe(df, df_previous) -> pd.DataFrame:
     """ This function it takes a new and a old dataset.
 
     It serves to trace the price changes on the platform.
 
     """
-    if df_old is None:  # First-time scrape
-        df_new['First Scrape Date'] = dt.datetime.now()\
+    if df_previous.empty:  # First-time scrape
+        df['First Scrape Date'] = dt.datetime.now()\
             .strftime('%Y-%m-%d %H:%M:%S')
-        df_new['Current Scrape Date'] = df_new['First Scrape Date']
-        df_new['Original Price (IDR)'] = df_new['Price (IDR)']
+        df['Current Scrape Date'] = df['First Scrape Date']
+        df['Original Price (USD)'] = df['Price (USD)']
+        df['Original Price (IDR)'] = df['Price (IDR)']
     else:  # Subsequent scrapes
-        df_new['Current Scrape Date'] = dt.datetime.now()\
+        print("Old already exists")
+        df['Current Scrape Date'] = dt.datetime.now()\
             .strftime('%Y-%m-%d %H:%M:%S')
         # Merge old and new data
         df_merged = pd.merge(
-            df_new,
-            df_old[[
+            df,
+            df_previous[[
                 'Code',
                 'First Scrape Date',
+                'Original Price (USD)'
                 'Original Price (IDR)']], on='Code', how='left')
-        # Update 'First Scrape Date' and 'Original Price (IDR)' for new entries
-        df_merged['First Scrape Date'].fillna(
-            df_new['Current Scrape Date'], inplace=True)
-        df_merged['Original Price (IDR)'].fillna(
-            df_new['Price (IDR)'], inplace=True)
-        df_new = df_merged
 
-    return df_new
+        df = df_merged
+
+    return df
 
 
-def obtain_links(base_url, website_page) -> list:
+def obtain_links(page, base_url, website_page) -> list:
     """ This function returns a list of properties links that are displayed
     in a page.
 
@@ -108,7 +96,7 @@ def obtain_links(base_url, website_page) -> list:
     return property_links
 
 
-def change_currency_n_get_soup(property_link) -> object:
+def change_currency_n_get_soup(page, property_link) -> object:
     """ This function takes the property link and returns a beautiful soup
     element.
 
@@ -150,9 +138,7 @@ def get_shared_features(soup):
 
     """
     # get date using the image
-    images = soup.select('figure')
-    img_tags = images[0].find_all('img')
-    img_src = img_tags[0].get('src')
+    img_src = soup.select('figure')[0].find_all('img')[0].get('src')
     date = img_src.split('/')[-1].split('-property')[0]
     pattern = r"^\d{4}-\d{2}-\d{2}$"
 
@@ -167,10 +153,10 @@ def get_shared_features(soup):
 
     # get sale type for each property
     type_sale = colswidth20_item[1] \
-        .split('\n')[2].split(' ')[0]
+        .split('\n')[2].split(' ')[0] + "hold"
 
     # get years if lease
-    if type_sale == 'lease':
+    if type_sale == 'leasehold':
         hold_years = colswidth20_item[1] \
             .split('\n')[3].split('/ ')[1]
         hold_years = hold_years.split(' y')[0]
@@ -192,7 +178,7 @@ def get_shared_features(soup):
     return type_sale, hold_years, description_items, colswidth20_item, date
 
 
-def get_only_villas_features(soup, description_items, prices):
+def get_only_villas_features(soup, description_items, prices, prices_usd):
     """ This function returns only the elements that are specific for villas.
     They might be the same as the ones for lands, but the scraping process
     changes.
@@ -215,7 +201,7 @@ def get_only_villas_features(soup, description_items, prices):
         for paragraph in items:
             rooms.append(paragraph.text.strip())
 
-    # if it findd the "Year Built" add it
+    # if it finds the "Year Built" add it
     if 'Year Built' in description_items[5]:
         year_built = description_items[5]
         year_built = year_built.split(': ')[1]
@@ -223,16 +209,27 @@ def get_only_villas_features(soup, description_items, prices):
             .split('\n')[1].strip()
         building_size = description_items[6]\
             .split('\n')[1].strip()
-        furnished = description_items[7]\
-            .split('\n')[1].strip()
+        try:
+            furnished = description_items[7]\
+                .split('\n')[1].strip().lower()
+        except Exception as error:
+            print(error)
+            furnished = None
+            print('furnished fixed!')
+
     else:
         year_built = None
         land_size = description_items[3]\
             .split('\n')[1].strip()
         building_size = description_items[5]\
             .split('\n')[1].strip()
-        furnished = description_items[6]\
-            .split('\n')[1].strip()
+        try:
+            furnished = description_items[6]\
+                .split('\n')[1].strip()
+        except Exception as error:
+            print(error)
+            furnished = None
+            print('furnished fixed!')
 
     # check for available facilities
     facilities = soup.select('.flexbox-wrap')
@@ -250,18 +247,21 @@ def get_only_villas_features(soup, description_items, prices):
 
     # clean price variable
     try:
+        price_usd = [price_usd.text.strip().split(' ')[1]
+                     .replace(',', '') for price_usd in prices_usd][0]
         price = [price.text.strip().split(' ')[1]
                  .replace(',', '') for price in prices][0]
     except Exception as error:
         print(error)
         price = [price.text.strip() for price in prices][0]
+        price_usd = [price_usd.text.strip() for price_usd in prices_usd][0]
 
-    return price,\
+    return price, price_usd,\
         year_built, land_size, building_size, \
         pool, furnished, rooms
 
 
-def get_only_lands_features(prices) -> str:
+def get_only_lands_features(prices, prices_usd) -> str:
     """ This function returns only the elements that are specific for lands.
     They might be the same as the ones for villas, but the scraping process
     changes.
@@ -272,19 +272,24 @@ def get_only_lands_features(prices) -> str:
     """
 
     try:
-        price = [price.text.strip().split('R ')[1]
+        price = [price.text.strip().split(' ')[1]
                  .split('\n')[0]
                  .strip()
                  .replace(',', '') for price in prices][0]
+        price_usd = [price_usd.text.strip().split(' ')[1]
+                     .split('\n')[0]
+                     .strip()
+                     .replace(',', '') for price_usd in prices_usd][0]
 
     except Exception as error:
         print(error)
         price = [price.text.strip() for price in prices][0]
+        price_usd = [price_usd.text.strip() for price_usd in prices_usd][0]
 
-    return price
+    return price, price_usd
 
 
-def scraper(url, n_pages=90) -> pd.DataFrame:
+def scraper(page, url, n_pages=90) -> pd.DataFrame:
     """ This function takes all the other functions to build the scraping
     process.
 
@@ -296,13 +301,23 @@ def scraper(url, n_pages=90) -> pd.DataFrame:
     """
     details = []
 
-    for website_page in range(n_pages):
+    for website_page in range(92, n_pages):
         print('page: ', website_page)
-        property_links = obtain_links(url, website_page)
+        property_links = obtain_links(page, url, website_page)
         for link in property_links:
             try:
+                # go to new url provided by the link
+                page.goto(link)
+
+                html = page.inner_html('body')
+
+                soup = BeautifulSoup(html, 'html.parser')
+
+                # get prices in usd
+                prices_usd = soup.select('.regular-price')
+
                 # make a beautiful soup object
-                soup = change_currency_n_get_soup(link)
+                soup = change_currency_n_get_soup(page, link)
 
                 # get the price of each property
                 prices = soup.select('.regular-price')
@@ -323,14 +338,15 @@ def scraper(url, n_pages=90) -> pd.DataFrame:
                     date = get_shared_features(soup)
 
                 if 'villas' in url:
-                    price, year_built, land_size, building_size,\
+                    price, price_usd, year_built, land_size, building_size,\
                         pool, furnished, rooms = get_only_villas_features(
-                            soup, description_items, prices)
+                            soup, description_items, prices, prices_usd)
 
                     detail = {
                         'Title': [
                             title.text.strip() for title in titles][0],
                         'Upload Date': date,
+                        'Price (USD)': price_usd,
                         'Price (IDR)': price,
                         'Code': [
                             code.text.strip() for code in codes][0],
@@ -346,18 +362,21 @@ def scraper(url, n_pages=90) -> pd.DataFrame:
                         'Land Size (are)': land_size,
                         'Building Size (sqm)': building_size,
                         'Pool': pool,
-                        'Furnished': furnished.lower()
+                        'Furnished': furnished
                     }
 
                     details.append(detail)
 
                 else:
-                    price = get_only_lands_features(prices)
+                    price, price_usd = get_only_lands_features(
+                        prices,
+                        prices_usd)
 
                     detail = {
                         'Title': [
                             title.text.strip() for title in titles][0],
                         'Upload Date': date,
+                        'Price (USD)': price_usd,
                         'Price (IDR)': price,
                         'Code': [
                             code.text.strip() for code in codes][0],
@@ -381,14 +400,65 @@ def scraper(url, n_pages=90) -> pd.DataFrame:
 
             except Exception as error:
                 print(error)
+                print(link)
             continue
 
     return pd.DataFrame(details)
 
 
+def main(url_lands, url_villas):
+    """ Main function """
+    # "with" statement for exception handling
+    with sync_playwright() as pw:
+        # creates an instance of the Chromium browser and launches it
+        browser = pw.chromium.launch(headless=True)
+        # creates a new browser page (tab) within the browser instance
+        page = browser.new_page()
+
+        # Get last page to iterate
+        num_lands = get_last_page_number(page, url_lands)
+        num_villas = get_last_page_number(page, url_villas)
+
+        try:
+            # Authenticate Google Sheet and open the worksheet
+            worksheet = google_authentication(
+                f"{PATH}/credentials.json",
+                os.getenv('SHEET_ID'))
+
+            # Check for old data in the Google
+            df_old = read_from_google(worksheet)
+
+            # Scrape new data
+            df_lands = scraper(
+                page,
+                url=URL_LANDS,
+                n_pages=num_lands - num_lands)
+            df_villas = scraper(
+                page,
+                url=URL_VILLAS,
+                n_pages=num_villas-1)
+
+            # Merge both
+            df_new = pd.concat([df_villas, df_lands])
+
+            # Update dataframe
+            df_new = update_dataframe(df_new, df_old)
+
+            # apply reorder
+            df_new = df_new[column_order]
+
+            # Upload to Google Sheet with new information
+            upload_to_google(df_new, worksheet)
+
+        finally:
+            page.close()
+
+
 if __name__ == '__main__':
     # path of the scraper
-    PATH = os.path.dirname(__file__)
+    PATH = os.path.dirname(os.path.dirname(__file__))
+
+    print(PATH)
 
     # load environment keys
     load_dotenv(f"{PATH}/keys.env")
@@ -404,6 +474,8 @@ if __name__ == '__main__':
         'Upload Date',
         'First Scrape Date',
         'Current Scrape Date',
+        'Original Price (USD)',
+        'Price (USD)',
         'Original Price (IDR)',
         'Price (IDR)',
         'Location',
@@ -419,47 +491,5 @@ if __name__ == '__main__':
         'Pool',
         'Furnished']
 
-    # "with" statement for exception handling
-    with sync_playwright() as p:
-        # creates an instance of the Chromium browser and launches it
-        browser = p.chromium.launch(headless=True)
-        # creates a new browser page (tab) within the browser instance
-        page = browser.new_page()
-
-        try:
-            # Load existing data if available
-            try:
-                df_lands_old = pd.read_csv(f"{PATH}/data/lands.csv")
-                df_villas_old = pd.read_csv(f"{PATH}/data/villas.csv")
-            except FileNotFoundError:
-                df_lands_old = None
-                df_villas_old = None
-
-            # Scrape new data
-            df_lands_new = scraper(url=URL_LANDS, n_pages=34)
-            df_villas_new = scraper(url=URL_VILLAS, n_pages=94)
-
-            # Update dataframes
-            df_lands = update_dataframe(df_lands_new, df_lands_old)
-            df_villas = update_dataframe(df_villas_new, df_villas_old)
-
-            # apply reorder
-            df_lands = df_lands[column_order]
-            df_villas = df_villas[column_order]
-
-            # Merge both
-            df_data = pd.concat([df_villas, df_lands])
-
-            # Save all datasets as .csv files
-            df_lands.to_csv(f"{PATH}/data/lands.csv", index=False)
-            df_villas.to_csv(f"{PATH}/data/villas.csv", index=False)
-            df_data.to_csv(f"{PATH}/data/kib_data.csv", index=False)
-
-        finally:
-            page.close()
-
-    # upload merged dataframe to a google sheet
-    upload_to_google(
-        f"{PATH}/credentials.json",
-        os.getenv('SHEET_ID'),
-        df_data)
+    # run main function
+    main(URL_LANDS, URL_VILLAS)
